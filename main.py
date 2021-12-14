0977534d9a85f81f37e9f840600fe1b6455a5256
Daniel_Hajduk <daniel.hajduk@infermedica.com>
2021-12-14 20:33:14 +0100

    Reformat code and move search and topsstreets to src

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from src.cli import get_function, get_searched_phrase
-from search import search
-from topStreets import topStreets
+from src.search import search
+from src.topStreets import top_streets
 from sys import argv
 
 function = get_function(argv)
 
 if function == "search":
-	searched_street = get_searched_phrase(argv)
-	search(searched_street)
+    searched_street = get_searched_phrase(argv)
+    search(searched_street)
 if function == "top":
-	topStreets()
+    top_streets()
--- src/places.py
+++ src/places.py
@@ -8,7 +8,7 @@ class City(object):
 
 class Street(object):
     def __init__(self, properties):
-        self.voivodeship_id = properties.split(";")[0]        
+        self.voivodeship_id = properties.split(";")[0]
         self.prefix = properties.split(";")[6]
         self.additional_name = properties.split(";")[8]
         self.proper_name = properties.split(";")[7]
@@ -21,3 +21,10 @@ class Street(object):
 
     def set_city(self, city):
         self.city = city
+
+
+class Voivodeship(object):
+
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
