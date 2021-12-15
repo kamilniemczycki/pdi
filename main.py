890be41c7e7c1bb93ff1866e8101f34fc2541a2b
Kamil Niemczycki <kamilniemczycki@gmail.com>
2021-12-15 13:14:12 +0100

    The top hat for cities for the country has been added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.cli import get_function, get_searched_phrase
 from src.search import search
 from src.top_streets.per_voivodeship import TopStreetsPerVoivodeship
+from src.top_streets.in_country import TopStreetsInCountry
 from sys import argv
 
 function = get_function(argv)
@@ -10,3 +11,5 @@ if function == "search":
     search(searched_street)
 if function == "top":
     TopStreetsPerVoivodeship().print()
+if function == "top_country":
+    TopStreetsInCountry().print()
