e3ab8efb67d543e7af5213e44f1eef18df7ba164
Kamil Niemczycki <kamilniemczycki@gmail.com>
2022-01-11 00:43:08 +0100

    Renamed file street_per_voivodeship.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.cli import get_function, get_searched_phrase
 from src.search import search
-from src.top_streets.per_voivodeship import TopStreetsPerVoivodeship
+from src.top_streets.street_per_voivodeship import TopStreetsPerVoivodeship
 from src.top_streets.in_country import TopStreetsInCountry
 from src.top_streets.popular_cities import PopularCities
 from src.top_streets.duplicated_street import DuplicatedStreet
